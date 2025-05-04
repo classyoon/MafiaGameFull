7e8cd53f882b1732517d20376f770f8d3b6500f8
classyoon <96508736+classyoon@users.noreply.github.com>
2025-05-03 23:24:31 -0400

    Initial Commit

---
+++ MafiaGameFull/MafiaGameFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  MafiaGameFullApp.swift
+//  MafiaGameFull
+//
+//  Created by Conner Yoon on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MafiaGameFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MafiaGameFullTests/MafiaGameFullTests.swift
@@ -0,0 +1,16 @@
+//
+//  MafiaGameFullTests.swift
+//  MafiaGameFullTests
+//
+//  Created by Conner Yoon on 5/3/25.
+//
+
+import Testing
+
+struct MafiaGameFullTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
